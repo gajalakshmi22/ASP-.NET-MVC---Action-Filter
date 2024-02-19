a0c9e740c8e7360d85b304808f958d8301e3ea0d
gajalakshmi22 <gaja.sightspectrum@gmail.com>
2024-02-19 15:41:31 +0530

    name

---
+++ DemoFile/Common/PartialCacheAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Configuration;
+using System.Configuration;
+
+namespace DemoFile.Common
+{
+    public class PartialCacheAttribute : OutputCacheAttribute
+    {
+        public PartialCacheAttribute(string cacheProfileName)
+        {
+            OutputCacheSettingsSection outputCacheSettingsSection =
+            (OutputCacheSettingsSection) WebConfigurationManager.GetSection("system.web/caching/outputCacheSettings");
+            OutputCacheProfile outputCacheProfile = outputCacheSettingsSection.OutputCacheProfiles[cacheProfileName];
+            Duration = outputCacheProfile.Duration;
+            VaryByParam = outputCacheProfile.VaryByParam;
+        }
+    }
+}
---
+++ DemoFile/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoFile.MvcApplication" Language="C#" %>
